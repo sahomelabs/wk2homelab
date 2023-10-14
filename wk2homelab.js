09322b3d24101f47e61b07caac701ebf95090ed4
sahomelabs <sahomelabs@gmail.com>
2023-10-14 17:55:27 -0400

    Complete Lab1

---
+++ wk2homelab.js
@@ -0,0 +1,14 @@
+// Write an if/else statement for the following requirements
+let grade = 80
+  if (grade >= 80) {
+    console.log("You did a good job");
+  } else if (grade >= 70) {
+    console.log("Keep trying");
+  } else if (grade >= 60) {
+    console.log("ehhhh");
+  } else if (grade >= 55) {
+    console.log("Not too good") ;
+  } else {
+    console.log("Bad Grade But You'll Get Em Next Time");
+  }
+
