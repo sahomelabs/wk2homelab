f0f01202a46b49b992692f6adab52a8a8b6bf07e
sahomelabs <sahomelabs@gmail.com>
2023-10-14 19:41:42 -0400

    Complete Lab 5

--- wk2homelab.js
+++ wk2homelab.js
@@ -1,14 +1,30 @@
 // Write an if/else statement for the following requirements
-let grade = 80
-  if (grade >= 80) {
-    console.log("You did a good job");
-  } else if (grade >= 70) {
-    console.log("Keep trying");
-  } else if (grade >= 60) {
-    console.log("ehhhh");
-  } else if (grade >= 55) {
-    console.log("Not too good") ;
-  } else {
-    console.log("Bad Grade But You'll Get Em Next Time");
-  }
+// let grade = 80
+//   if (grade >= 80) {
+//     console.log("You did a good job");
+//   } else if (grade >= 70) {
+//     console.log("Keep trying");
+//   } else if (grade >= 60) {
+//     console.log("ehhhh");
+//   } else if (grade >= 55) {
+//     console.log("Not too good") ;
+//   } else {
+//     console.log("Bad Grade But You'll Get Em Next Time");
+//   }
 
+
+// 5. Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. 
+// For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. 
+// When you have that working, modify your program to print "FizzBuzz", 
+// for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+//  for (let i = 1; i <= 100; i ++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz')
+//     }else {
+//         console.log(i);
+//     }
+//  }
