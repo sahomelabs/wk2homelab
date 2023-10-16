cdceb1166564b8d9616cbfb6aae7ca9c79849243
sahomelabs <sahomelabs@gmail.com>
2023-10-16 19:27:47 -0400

    Update function for lab1 and lab2

--- wk2homelab.js
+++ wk2homelab.js
@@ -1,5 +1,6 @@
 // Write an if/else statement for the following requirements
-// let grade = 80
+
+// function graderesult(grade) {
 //   if (grade >= 80) {
 //     console.log("You did a good job");
 //   } else if (grade >= 70) {
@@ -11,7 +12,29 @@
 //   } else {
 //     console.log("Bad Grade But You'll Get Em Next Time");
 //   }
+// }
+// graderesult(57);
 
+// //2. Write function that prints out mutiple of 10 up to a given input (argument)
+// function printmutiplesoften(numbers) {
+//     for (let i = 10; i <= numbers; i += 10) {
+//         console.log(i);
+
+//     }
+// }
+// printmutiplesoften(50);
+
+ 
+
+
+// 3. Write a function that takes bill amount and item price and says how many quarters they'd get in return
+// let remainder = 6;
+// let quarters = 0;
+// function countQuarters(billAmount,itemPrice){
+//     let totalCost = billAmount - itemPrice;
+//     let numQuarters = Math.floor(totalCost / .25);
+//     console.log(`You would receive ${numQuarters} quarters`);
+//     }
 
 // 5. Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. 
 // For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. 
@@ -28,3 +51,5 @@
 //         console.log(i);
 //     }
 //  }
+
+
